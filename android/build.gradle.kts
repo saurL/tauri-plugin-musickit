3e5b17245e402a6c12ea0e2dfc2b89f2af61df16
Lucas Sauron <sauronlucas@gmail.com>
2025-08-29 17:34:00 +0200

    a

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -60,8 +60,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(project(":tauri-android"))
-    implementation("musickitauth-release-1.1.2") {
-        ext = "aar"
-    }
-
+    implementation(":musickitauth-release-1.1.2@aar")
 }
